2dcbc331a30cc882a51678aca5c345c162869b60
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-12-23 00:22:22 +0200

    added a very basic sdl window

--- apps/CChess.c
+++ apps/CChess.c
@@ -1,4 +1,16 @@
+#include <SDL2/SDL.h>
+
 int main(int argc, char *argv[])
 {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          680, 480,
+                                          0);
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(5000);
     return 0;
 }
