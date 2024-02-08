2903fd67bc613e986b97e8aa801e88b47256d948
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2024-02-08 17:13:48 +0200

    added background and self-drawn temporary pieces (used for testing) {

--- apps/CChess.c
+++ apps/CChess.c
@@ -1,4 +1,7 @@
 #include <SDL2/SDL.h>
+
+#include <stdbool.h>
+
 #include "project/piece.h"
 #include "project/table.h"
 
@@ -10,13 +13,37 @@ int main(int argc, char *argv[])
 
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+
+    SDL_Window *window = SDL_CreateWindow("CChess",
                                           SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED,
-                                          680, 480,
+                                          800, 600,
                                           0);
-    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(5000);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Surface *background = SDL_LoadBMP("../data/background.bmp");
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, background);
+    bool quit = false;
+    SDL_Event event;
+    while (!quit)
+    {
+        SDL_WaitEvent(&event);
+
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                quit = true;
+                break;
+        }
+
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(background);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
