235407bf87ee3fc2ab079b8956765891da846706
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2024-02-12 14:22:35 +0200

    moved from .c to .h

--- lib/ChessEngine/include/pieceRules.h
+++ lib/ChessEngine/include/pieceRules.h
@@ -1,4 +1,41 @@
 #ifndef CCHESS_PIECE_RULES_H
 #define CCHESS_PIECE_RULES_H
 
+#include "piece.h"
+
+typedef enum
+{
+    MOVE,
+    CAPTURE,
+    CASTLE,
+    PROMOTE,
+    EN_PASSANT
+
+}MoveType_e;
+typedef struct
+{
+    struct
+    {
+        int x;
+        int y;
+        MoveType_e type;
+
+        // to make a move we might need information about another piece.
+        // for example:
+        //  1) With respect to the King, if we decide to castle we must
+        //  know where the rook is
+        //  2) With respect to the Pawn, if we use en'passant, it might
+        //  help us to know what pawn was attacked.
+        //  3) Maybe it would be better for every capture to remember which piece
+        //  we are attacking. It is redundant information as we can extract it from
+        //  the move position.
+
+        Piece *movePartner;
+
+    } moves[64]; //there are 64 spots on the table, so we can't have
+    //more than 64 valid spots for a piece to move in
+    int noMoves;
+    Piece *subjectPiece;
+}LegalMoves;
+
 #endif //CCHESS_PIECE_RULES_H
